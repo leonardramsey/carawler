dc087d9ee600ef346603876ea98edce60a8f4e35
Leonard Ramsey <lr3hj@virginia.edu>
2018-07-11 23:04:05 -0400

    Working on adding filtering

--- carawler/carawler/spiders/sales.py
+++ carawler/carawler/spiders/sales.py
@@ -6,13 +6,35 @@ from scrapy import Request
 class SalesSpider(scrapy.Spider):
     name = 'sales'
     allowed_domains = ['craigslist.org']
-    start_urls = ['https://norfolk.craigslist.org/search/cta',
-                  'https://richmond.craigslist.org/search/cta',
-                  'https://washingtondc.craigslist.org/search/cta',
-                  'https://charlottesville.craigslist.org/search/cta',
-                  'https://winchester.craigslist.org/search/cta',
-                  'https://baltimore.craigslist.org/search/cta',
-                  'https://fredericksburg.craigslist.org/search/cta']
+
+    # ?max_price=11000&min_auto_year=2008&max_auto_year=2012&max_auto_miles=130000
+    def __init__(self, min_price=None, max_price=None, min_auto_year=None, max_auto_year=None, max_auto_miles=None, **kwargs):
+        filter_string = '?'
+        if min_price:
+            filter_string = filter_string + 'min_price=' + str(min_price).strip() + '&'
+        if max_price:
+            filter_string = filter_string + 'max_price=' + str(max_price).strip() + '&'
+        if min_auto_year:
+            filter_string = filter_string + 'min_auto_year=' + str(min_auto_year).strip() + '&'
+        if max_auto_year:
+            filter_string = filter_string + 'max_auto_year=' + str(max_auto_year).strip() + '&'
+        if max_auto_miles:
+            filter_string = filter_string + 'max_auto_miles=' + str(max_auto_miles).strip() + '&'
+        # if no filters supplied, then make filter string empty
+        if filter_string == '?':
+            filter_string = ''
+        # remove last '&' from string
+        else:
+            filter_string = filter_string[:-1]
+        self.start_urls = ['https://norfolk.craigslist.org/search/cta%s' % filter_string,
+                          'https://richmond.craigslist.org/search/cta%s' % filter_string,
+                          'https://washingtondc.craigslist.org/search/cta%s' % filter_string,
+                          'https://charlottesville.craigslist.org/search/cta%s' % filter_string,
+                          'https://winchester.craigslist.org/search/cta%s' % filter_string,
+                          'https://baltimore.craigslist.org/search/cta%s' % filter_string,
+                          'https://fredericksburg.craigslist.org/search/cta%s' % filter_string]
+        super(SalesSpider, self).__init__(**kwargs)
+        self.log(self.domain)
 
     def parse(self, response):
         item = {}
